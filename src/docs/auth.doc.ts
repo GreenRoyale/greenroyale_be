ab39a23cafa2b4c2675de3d098eeab21a6a7efcc
Wasiu Bakare <wasiubakare@gmail.com>
2024-09-05 14:51:48 +0100

    feat: add api docs for create user

---
+++ src/docs/auth.doc.ts
@@ -0,0 +1,89 @@
+export const defaultSchema = `
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     UserSignUp:
+ *       type: object
+ *       required:
+ *         - first_name
+ *         - last_name
+ *         - email
+ *         - password
+ *       properties:
+ *         first_name:
+ *           type: string
+ *           description: First name of the user
+ *         last_name:
+ *           type: string
+ *           description: Last name of the user
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: Email of the user
+ *         password:
+ *           type: string
+ *           format: password
+ *           description: Password for the account
+ */
+`;
+
+export const register = `
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserSignUp'
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     user:
+ *                       $ref: '#/components/schemas/UserSignUp'
+ *       400:
+ *         description: Bad request
+ *       422:
+ *         description: Unprocessable entity (Validation error)
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: error
+ *                 message:
+ *                   type: string
+ *                   example: Validation failed
+ *                 errors:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       field:
+ *                         type: string
+ *                         example: email
+ *                       message:
+ *                         type: string
+ *                         example: "Email is required"
+ */
+
+`;
