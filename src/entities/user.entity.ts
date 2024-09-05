1c6d5e0a8c7e51588482b958bc916cba390e1303
Wasiu Bakare <wasiubakare@gmail.com>
2024-09-05 13:39:21 +0100

    fix: migration error

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity } from "typeorm";
+import ExtendedBaseEntity from "./base.entity";
+
+@Entity()
+class User extends ExtendedBaseEntity {
+  @Column({ nullable: false })
+  first_name: string;
+
+  @Column({ nullable: false })
+  last_name: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ default: false })
+  is_verified: boolean;
+}
+
+export { User };
