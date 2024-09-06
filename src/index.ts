14fcb6ca716ef9f56622fa54f2d45f661ba76c7b
masterchief-Dave <bodunrindavidbond@gmail.com>
2024-09-06 04:23:55 +0100

    feat: background queue

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import config from "config";
 import "reflect-metadata";
+import config from "../config/default";
 import app from "./app";
 import AppDataSource from "./datasource";
 import log from "./utils/logger";
 
-const port = config.get<number>("port");
+const port = config.port;
 
 AppDataSource.initialize()
-  .then(async () => {
-    app.listen(port, () => {
-      log.info(`App is listening on port ${port}`);
-    });
+  .then(async (data) => {
+    if (data.isInitialized) {
+      log.info("Database started");
+      app.listen(port, () => {
+        log.info(`App is listening on port ${port}`);
+      });
+    }
   })
   .catch((error) => log.error(error));
