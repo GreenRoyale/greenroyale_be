28ee0a883374500fc2cf6777513f33598329ac22
masterchief-Dave <bodunrindavidbond@gmail.com>
2024-09-09 05:05:36 +0100

    chore: user services tests

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { ClientError } from "../exceptions/clientError";
 import asyncHandler from "../middlewares/asyncHander";
 import { UserService } from "../services/user.service";
 import { BaseReponseHandler } from "./base-controllers/base-response-handler.controller";
@@ -8,15 +9,17 @@ const responseHandler = new BaseReponseHandler();
 
 const updateProfilePicture = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    // TODO: REPLACE USER_ID WITH DATA COMING FROM AUTH_MIDDLEWARE
-    const userId = "";
+    const userId = req.user.id;
     const payload = req.body;
+    if (!userId || !payload) {
+      return next(new ClientError("Missing required parameters"));
+    }
     const response = await userService.updateUserProfilePicture({
       payload,
       userId,
     });
 
-    return responseHandler.sendSuccessResponse(res, "Profile pictue updated", {
+    return responseHandler.sendSuccessResponse(res, "Profile picture updated", {
       data: response,
     });
   },
@@ -24,10 +27,17 @@ const updateProfilePicture = asyncHandler(
 
 const updateUserProfile = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    const userId = "";
+    const userId = req.user.id;
     const payload = req.body;
 
-    const response = await userService;
+    const response = await userService.updateUserProfile({
+      payload,
+      userId,
+    });
+
+    return responseHandler.sendSuccessResponse(res, "Profile updated", {
+      data: response,
+    });
   },
 );
 
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,34 +1,28 @@
-import { NextFunction, Router } from "express";
+import { Router } from "express";
 import {
   updateProfilePicture,
   updateUserProfile,
 } from "../controllers/user.controller";
+import { deserializeUser } from "../middlewares/deserializeUser";
 import { validateData } from "../middlewares/validateData";
 import {
   updateProfilePictureSchema,
   updateUserProfileSchema,
 } from "../schemas/user";
-import log from "../utils/logger";
 
 const userRouter = Router();
 
 userRouter.patch(
   "/profile-picture",
   validateData(updateProfilePictureSchema),
-  (_, res, next: NextFunction) => {
-    log.info("AUTH MIDDLEWARE SHOULD BE HERE!");
-    next();
-  },
+  deserializeUser,
   updateProfilePicture,
 );
 
 userRouter.patch(
   "/",
   validateData(updateUserProfileSchema),
-  (_, res, next: NextFunction) => {
-    log.info("AUTH MIDDLEWARE SHOULD BE HERE!");
-    next();
-  },
+  deserializeUser,
   updateUserProfile,
 );
 
